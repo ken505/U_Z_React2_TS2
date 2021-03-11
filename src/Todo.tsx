47dbfd27d704c1c9f72d512bacff9fb64b4db74d
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-11 20:51:45 +0900

    wip:

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,5 @@
 export const Todo = (props) => {
-  const { title, userid } = props;
-  return <P>{`${title}(user:${userid})`}</P>;
+  const { title, userid, completed } = props;
+  const completeMark = completed ? "[Done]" : "[Un]";
+  return <p>{`${completeMark}${title}(user:${userid})`}</p>;
 };
